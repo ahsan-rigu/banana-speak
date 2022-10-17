9f8a3445be23294b89d0059f1fc3546a2e9ec4d9
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2022-10-18 03:53:28 +0530

    script completed

---
+++ script.js
@@ -0,0 +1,30 @@
+var textInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var textOutput = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    alert("something is wrong, i really dont know what")
+}
+
+function clickEventHandler(){
+
+     var inputText = textInput.value;
+     fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            
+        })
+        .catch(errorHandler);
+
+
+ };
+ 
+ btnTranslate.addEventListener("click", clickEventHandler);
+ 
